c228d3adc47b1a5c375b120a7ed940fa87132859
Laurent Ghirardotti <laurent@ghirardotti.fr>
2021-12-07 16:45:28 +0100

    init

---
+++ content_script.js
@@ -0,0 +1,6 @@
+function clickBalm() {
+    document.querySelector('label.totalpoll-question-choices-item:nth-child(4)').querySelector('input').click();
+    document.querySelector('.totalpoll-buttons').querySelector('button').click();
+}
+
+clickBalm();
